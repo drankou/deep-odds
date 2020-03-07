a437fc5e1ea1c3141637a11615e39d212944b75c
Aliaksandr Drankou <alex@turing-technology.com>
2020-03-07 22:47:06 +0100

    *update* mongo tests refactored

--- src/betsapi_scrapper/storage/mongo_test.go
+++ src/betsapi_scrapper/storage/mongo_test.go
@@ -2,9 +2,12 @@ package storage
 
 import (
 	"betsapi_scrapper/types"
-	"encoding/json"
+	"betsapi_scrapper/utils"
+	"go.mongodb.org/mongo-driver/bson"
+	"io/ioutil"
 	"log"
 	"os"
+	"path"
 	"reflect"
 	"testing"
 )
@@ -19,60 +22,32 @@ func TestMongoWrapper_Connect(t *testing.T) {
 
 func TestMongoWrapper_ReadOne(t *testing.T) {
 	os.Setenv("MONGO_CONNECTION_STRING", "mongodb://localhost:27017")
-	mongo := MongoWrapper{}
-	mongoConnectionString := os.Getenv("MONGO_CONNECTION_STRING")
-
-	mongo.Connect(mongoConnectionString, DefaultMongoDatabase)
+	mongo := GetMongoWrapper()
 
 	filter := make(map[string]interface{})
 	data, err := mongo.ReadOne("football_event", reflect.TypeOf(types.FootballEvent{}), filter)
-	if err != nil{
+	if err != nil {
 		t.Fatal(err)
 	}
 
-	jsonOld, err := json.Marshal(data)
-	if err != nil{
-		t.Fatal(err)
+	if _, ok := data.(*types.FootballEvent); !ok {
+		t.Fatal("unsuccessful type assertion")
 	}
-
-	log.Print(string(jsonOld))
-
-	var newFootballEvent types.NewFootballEvent
-
-	err = json.Unmarshal(jsonOld, &newFootballEvent)
-	if err != nil{
-		t.Fatal(err)
-	}
-
-	_, err = mongo.Insert("football_event", newFootballEvent)
-	if err != nil{
-		t.Fatal(err)
-	}
-
-	jsonNew, err := json.Marshal(newFootballEvent)
-	if err != nil{
-		t.Fatal(err)
-	}
-
-	log.Print(string(jsonNew))
 }
 
 func TestMongoWrapper_Insert(t *testing.T) {
 	os.Setenv("MONGO_CONNECTION_STRING", "mongodb://localhost:27017")
 	os.Setenv("ENVIRONMENT", "dev")
 
-	mongo := MongoWrapper{}
-	mongoConnectionString := os.Getenv("MONGO_CONNECTION_STRING")
-
-	mongo.Connect(mongoConnectionString, DefaultMongoDatabase)
+	mongo := GetMongoWrapper()
 
 	footballEvent := &types.FootballEvent{
 		Event: &types.Event{
-			HomeTeam: types.Team{
+			HomeTeam: &types.Team{
 				Name:        "Naftan Novopolotsk",
 				CountryCode: "by",
 			},
-			AwayTeam: types.Team{
+			AwayTeam: &types.Team{
 				Name:        "BATE Borisov",
 				CountryCode: "by",
 			},
@@ -89,12 +64,7 @@ func TestMongoWrapper_Insert(t *testing.T) {
 
 func TestMongoWrapper_ReadAll(t *testing.T) {
 	os.Setenv("MONGO_CONNECTION_STRING", "mongodb://localhost:27017")
-	os.Setenv("ENVIRONMENT", "prod")
-
-	mongo := MongoWrapper{}
-	mongoConnectionString := os.Getenv("MONGO_CONNECTION_STRING")
-
-	mongo.Connect(mongoConnectionString, DefaultMongoDatabase)
+	mongo := GetMongoWrapper()
 
 	filter := make(map[string]interface{})
 	footballEvents, err := mongo.GetFootballEvents(filter)
@@ -107,12 +77,7 @@ func TestMongoWrapper_ReadAll(t *testing.T) {
 
 func TestGetMongoWrapper_ReadAll_Where(t *testing.T) {
 	os.Setenv("MONGO_CONNECTION_STRING", "mongodb://localhost:27017")
-	os.Setenv("ENVIRONMENT", "prod")
-
-	mongo := MongoWrapper{}
-	mongoConnectionString := os.Getenv("MONGO_CONNECTION_STRING")
-
-	mongo.Connect(mongoConnectionString, DefaultMongoDatabase)
+	mongo := GetMongoWrapper()
 
 	filter := map[string]interface{}{
 		"stats_trend.yellow_cards": map[string]interface{}{
@@ -132,10 +97,7 @@ func TestMongoWrapper_StreamAll(t *testing.T) {
 	os.Setenv("MONGO_CONNECTION_STRING", "mongodb://localhost:27017")
 	os.Setenv("ENVIRONMENT", "prod")
 
-	mongo := MongoWrapper{}
-	mongoConnectionString := os.Getenv("MONGO_CONNECTION_STRING")
-
-	mongo.Connect(mongoConnectionString, DefaultMongoDatabase)
+	mongo := GetMongoWrapper()
 
 	filter := map[string]interface{}{
 		"stats_trend.attacks": map[string]interface{}{
@@ -147,9 +109,71 @@ func TestMongoWrapper_StreamAll(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	total := 0
 	for footballEvent := range eventsChan {
+		total++
 		log.Print(footballEvent.(*types.FootballEvent).Event.Id)
 	}
 
-	//t.Log("Total number of football events with stats: ", len(footballEvents))
+	t.Log("Total number of football events with stats: ", total)
+}
+
+func TestDumpBsonStatsTrendForMock(t *testing.T) {
+	os.Setenv("MONGO_CONNECTION_STRING", "mongodb://localhost:27017")
+	mongo := GetMongoWrapper()
+
+	filter := map[string]interface{}{
+		"stats_trend.attacks.home": map[string]interface{}{
+			"$ne": nil,
+		},
+	}
+
+	entry, err := mongo.ReadOne("football_event", reflect.TypeOf(types.FootballEvent{}), filter)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	footballEvent := entry.(*types.FootballEvent)
+	statsTrend := footballEvent.StatsTrend
+
+	//log.Print("EventId: ", footballEvent.Event.Id)
+	data, err := bson.Marshal(statsTrend)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(path.Join(utils.GetAbsPathToRoot(), "mock_data", "stats_trend.bson"), data, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDumpBsonOddsForMock(t *testing.T) {
+	os.Setenv("MONGO_CONNECTION_STRING", "mongodb://localhost:27017")
+	mongo := GetMongoWrapper()
+
+	filter := map[string]interface{}{
+		"odds": map[string]interface{}{
+			"$ne": nil,
+		},
+	}
+
+	entry, err := mongo.ReadOne("football_event", reflect.TypeOf(types.FootballEvent{}), filter)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	footballEvent := entry.(*types.FootballEvent)
+	odds := footballEvent.Odds
+
+	//log.Print("EventId: ", footballEvent.Event.Id)
+	data, err := bson.Marshal(odds)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(path.Join(utils.GetAbsPathToRoot(), "mock_data", "odds.bson"), data, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
