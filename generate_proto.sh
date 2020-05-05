64c7ad22a9b1f2af542f6e2d4a9718f95f3c510e
Aliaksandr Drankou <alex@turing-technology.com>
2020-05-05 16:56:50 +0200

    *update* betsapi proto generation updated

--- generate_proto.sh
+++ generate_proto.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-protoc -I=. -I=./vendor --gofast_out=plugins=grpc,paths=source_relative:. ./pkg/betsapi/types/types.proto
+protoc -I=. -I=$GOPATH/src --gofast_out=plugins=grpc,paths=source_relative:. ./pkg/betsapi/types/types.proto
